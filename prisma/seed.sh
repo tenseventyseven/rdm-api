70ed31ff360e5674eece8ec68cf0aa0c33553e7c
James U <tenseventyseven@gmail.com>
2024-10-03 13:37:41 +1000

    update seed script

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env bash
 
+BASE_URL=$1
+
+if [ -z "$BASE_URL" ]; then
+  echo "Usage: $0 <base-url>"
+  exit 1
+fi
+
 echo "Creating users"
-curl --location 'http://localhost:3000/users' --header 'Content-Type: application/json' --data '{ "userId": "u.ja" }'
-curl --location 'http://localhost:3000/users' --header 'Content-Type: application/json' --data '{ "userId": "yang.e" }'
-curl --location 'http://localhost:3000/users' --header 'Content-Type: application/json' --data '{ "userId": "milton.m" }'
+curl --location "${BASE_URL}/users" --header 'Content-Type: application/json' --data '{ "userId": "u.ja" }'
+curl --location "${BASE_URL}/users" --header 'Content-Type: application/json' --data '{ "userId": "yang.e" }'
+curl --location "${BASE_URL}/users" --header 'Content-Type: application/json' --data '{ "userId": "milton.m" }'
 
 echo "Creating projects"
-curl --location 'http://localhost:3000/projects' --header 'Content-Type: application/json' --data '{ "projectId": "P001" }'
-curl --location 'http://localhost:3000/projects' --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
+curl --location "${BASE_URL}/projects" --header 'Content-Type: application/json' --data '{ "projectId": "P001" }'
+curl --location "${BASE_URL}/projects" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
 
 echo "Creating datasets"
-curl --location 'http://localhost:3000/datasets' --header 'Content-Type: application/json' --data '{ "datasetId": "D001", "projectId": "P001" }'
-curl --location 'http://localhost:3000/datasets' --header 'Content-Type: application/json' --data '{ "datasetId": "D002", "projectId": "P001" }'
-curl --location 'http://localhost:3000/datasets' --header 'Content-Type: application/json' --data '{ "datasetId": "D003", "projectId": "P002" }'
+curl --location "${BASE_URL}/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D001", "projectId": "P001" }'
+curl --location "${BASE_URL}/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D002", "projectId": "P001" }'
+curl --location "${BASE_URL}/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D003", "projectId": "P002" }'
 
 echo "Associating users to projects"
-curl --location --request PUT 'http://localhost:3000/projects/1/users' --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "yang.e"] }'
-curl --location --request PUT 'http://localhost:3000/projects/2/users' --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "milton.m"] }'
+curl --location --request PUT "${BASE_URL}/projects/1/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "yang.e"] }'
+curl --location --request PUT "${BASE_URL}/projects/2/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "milton.m"] }'
 
 echo "Sharing a dataset with a project"
-curl --location --request POST 'http://localhost:3000/datasets/1/share' --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
+curl --location --request POST "${BASE_URL}/datasets/1/share" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
 
 echo "Creating instruments"
-curl --location 'http://localhost:3000/instruments' --header 'Content-Type: application/json' --data '{ "instrumentId": "I001", "displayName": "Microscope 001" }'
-curl --location 'http://localhost:3000/instruments' --header 'Content-Type: application/json' --data '{ "instrumentId": "I002", "displayName": "Microscope 002" }'
-curl --location 'http://localhost:3000/instruments' --header 'Content-Type: application/json' --data '{ "instrumentId": "I003", "displayName": "Sequencer 001" }'
-curl --location 'http://localhost:3000/instruments' --header 'Content-Type: application/json' --data '{ "instrumentId": "I004", "displayName": "Sequencer 002" }'
+curl --location "${BASE_URL}/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I001", "displayName": "Microscope 001" }'
+curl --location "${BASE_URL}/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I002", "displayName": "Microscope 002" }'
+curl --location "${BASE_URL}/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I003", "displayName": "Sequencer 001" }'
+curl --location "${BASE_URL}/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I004", "displayName": "Sequencer 002" }'
