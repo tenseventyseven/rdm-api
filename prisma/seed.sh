2add000413e4e6a84d4dc243febedee595b3e52f
James U <tenseventyseven@gmail.com>
2024-10-06 18:41:52 +1100

    fix seed

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -17,16 +17,19 @@ curl -X POST --location "${BASE_URL}/api/projects" --header 'Content-Type: appli
 curl -X POST --location "${BASE_URL}/api/projects" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
 
 echo "Creating datasets"
-curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D001", "projectId": "P001" }'
-curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D002", "projectId": "P001" }'
-curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": "D003", "projectId": "P002" }'
+D001=uuidgen
+D002=uuidgen
+D003=uuidgen
+curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '"$D001"', "projectId": "P001" }'
+curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '"$D002"', "projectId": "P001" }'
+curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '"$D003"', "projectId": "P002" }'
 
 echo "Associating users to projects"
 curl -X PUT --location "${BASE_URL}/api/projects/P001/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "yang.e", "milton.m"] }'
 curl -X PUT --location "${BASE_URL}/api/projects/P002/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "milton.m"] }'
 
 echo "Sharing a dataset with a project"
-curl -X POST --location "${BASE_URL}/api/datasets/D001/share" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
+curl -X POST --location "${BASE_URL}/api/datasets/${D001}/share" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
 
 echo "Creating instruments"
 curl -X POST --location "${BASE_URL}/api/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I001", "displayName": "Microscope 001" }'
