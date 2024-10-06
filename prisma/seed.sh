29c643c6c1b17ab6f52b2936e929b4b09973069e
James U <tenseventyseven@gmail.com>
2024-10-07 10:49:12 +1100

    update seed

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -x
-
 BASE_URL=$1
 
 if [ -z "$BASE_URL" ]; then
@@ -8,32 +6,43 @@ if [ -z "$BASE_URL" ]; then
   exit 1
 fi
 
-echo "Creating users"
+echo "---- Seeding data ----"
+
+echo "Creating users..."
 curl -X POST --location "${BASE_URL}/api/users" --header 'Content-Type: application/json' --data '{ "userId": "u.ja" }'
 curl -X POST --location "${BASE_URL}/api/users" --header 'Content-Type: application/json' --data '{ "userId": "yang.e" }'
 curl -X POST --location "${BASE_URL}/api/users" --header 'Content-Type: application/json' --data '{ "userId": "milton.m" }'
+echo "...done"
 
 echo "Creating projects"
 curl -X POST --location "${BASE_URL}/api/projects" --header 'Content-Type: application/json' --data '{ "projectId": "P001" }'
 curl -X POST --location "${BASE_URL}/api/projects" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
+echo "...done"
 
-echo "Creating datasets"
+echo "Creating datasets..."
 D001=$(uuidgen)
 D002=$(uuidgen)
 D003=$(uuidgen)
+mkdir /vast/projects/ResearchDataManagement/${D001}
+mkdir /vast/projects/ResearchDataManagement/${D002}
+mkdir /vast/projects/ResearchDataManagement/${D003}
 curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '\"$D001\"', "projectId": "P001" }'
 curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '\"$D002\"', "projectId": "P001" }'
 curl -X POST --location "${BASE_URL}/api/datasets" --header 'Content-Type: application/json' --data '{ "datasetId": '\"$D003\"', "projectId": "P002" }'
+echo "...done"
 
-echo "Associating users to projects"
+echo "Associating users to projects..."
 curl -X PUT --location "${BASE_URL}/api/projects/P001/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "yang.e", "milton.m"] }'
 curl -X PUT --location "${BASE_URL}/api/projects/P002/users" --header 'Content-Type: application/json' --data '{ "userIds": ["u.ja", "milton.m"] }'
+echo "...done"
 
-echo "Sharing a dataset with a project"
+echo "Sharing a dataset with a project..."
 curl -X POST --location "${BASE_URL}/api/datasets/${D001}/share" --header 'Content-Type: application/json' --data '{ "projectId": "P002" }'
+echo "...done"
 
-echo "Creating instruments"
+echo "Creating instruments..."
 curl -X POST --location "${BASE_URL}/api/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I001", "displayName": "Microscope 001" }'
 curl -X POST --location "${BASE_URL}/api/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I002", "displayName": "Microscope 002" }'
 curl -X POST --location "${BASE_URL}/api/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I003", "displayName": "Sequencer 001" }'
 curl -X POST --location "${BASE_URL}/api/instruments" --header 'Content-Type: application/json' --data '{ "instrumentId": "I004", "displayName": "Sequencer 002" }'
+echo "...done"
